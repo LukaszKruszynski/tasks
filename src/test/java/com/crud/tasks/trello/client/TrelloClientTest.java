7461d8b90cf74bc6e56101d6216653dada37ff18
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-09-03 16:45:34 +0200

    create class TrelloClientTest.java

---
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -0,0 +1,9 @@
+package com.crud.tasks.trello.client;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloClientTest {
+
+}
