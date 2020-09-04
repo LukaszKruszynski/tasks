5ac6b4f683e218d1a7b0ec22c72a16ec82d9e48d
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-09-04 17:58:32 +0200

    Swagger configuration

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/trello/")
 public class TrelloController {
