87bd05c40ca1c5ff7fa3b1838e469e46c7b8f115
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-09-05 20:27:30 +0200

    shouldSendMail() test

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService service;
+
+    @Mock
+    private JavaMailSender sender;
+
+    @Test
+    public void shouldSendMail() {
+        //Given
+        Mail mail = new Mail("test@mail.pl", "test", "test");
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setText(mail.getMessage());
+        simpleMailMessage.setTo(mail.getMailTo());
+        simpleMailMessage.setSubject(mail.getSubject());
+
+        //When
+        service.send(mail);
+
+        //Then
+        Mockito.verify(sender, Mockito.times(1)).send(simpleMailMessage);
+    }
+}
