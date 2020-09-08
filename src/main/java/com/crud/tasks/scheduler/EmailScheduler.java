74daa14df7f51596d7435fd2babb701d621ccec8
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-09-08 15:21:05 +0200

    EmailScheduler create and added @Override methods to TaskRepository

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -1,4 +1,36 @@
 package com.crud.tasks.scheduler;
 
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailScheduler {
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long count = taskRepository.count();
+        String numeral = "tasks";
+        if (count == 1) numeral = "task";
+        emailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "Currently in database you got: " + count + " " + numeral + ".", "")
+        );
+    }
 }
