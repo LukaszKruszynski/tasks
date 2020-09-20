66ebaa092cc596023a6daf7370c0f95c5a3c8ffa
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-09-20 14:16:05 +0200

    refactor TrelloCardMapperTestSuite with MockitoJUnitRunner

--- src/test/java/com/crud/tasks/mapper/trellomapper/TrelloCardMapperTestSuite.java
+++ src/test/java/com/crud/tasks/mapper/trellomapper/TrelloCardMapperTestSuite.java
@@ -5,16 +5,15 @@ import com.crud.tasks.domain.TrelloCardDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TrelloCardMapperTestSuite {
-    @Autowired
+    @Mock
     private TrelloCardMapper mapper;
     private TrelloCard trelloCard;
     private TrelloCardDto trelloCardDto;
@@ -24,31 +23,33 @@ public class TrelloCardMapperTestSuite {
     public void setup() {
         this.trelloCard = new TrelloCard("card test", "testing...",
                 "position in test!", "267ehbdhdshwe7");
-        this.trelloCardDto = new TrelloCardDto("card dto test","dto testing...",
-                "dto position in test!","823jen23j");
+        this.trelloCardDto = new TrelloCardDto("card test", "testing...",
+                "position in test!", "267ehbdhdshwe7");
     }
 
     @Test
     public void mapToCardDtoTest() {
         //Given
+        when(mapper.mapToCardDto(trelloCard)).thenReturn(trelloCardDto);
         //When
         TrelloCardDto trelloCardDtoAfterMap = mapper.mapToCardDto(trelloCard);
         //Then
-        assertEquals(trelloCardDtoAfterMap.getName(), trelloCard.getName());
-        assertEquals(trelloCardDtoAfterMap.getDescription(), trelloCard.getDescription());
-        assertEquals(trelloCardDtoAfterMap.getPos(), trelloCard.getPos());
-        assertEquals(trelloCardDtoAfterMap.getIdList(), trelloCard.getIdList());
+        assertEquals("card test", trelloCardDtoAfterMap.getName());
+        assertEquals("testing...", trelloCardDtoAfterMap.getDescription());
+        assertEquals("position in test!", trelloCardDtoAfterMap.getPos());
+        assertEquals("267ehbdhdshwe7", trelloCardDtoAfterMap.getIdList());
     }
 
     @Test
     public void mapToCardTest() {
         //Given
+        when(mapper.mapToCard(trelloCardDto)).thenReturn(trelloCard);
         //When
         TrelloCard trelloCardAfterMap = mapper.mapToCard(trelloCardDto);
         //Then
-        assertEquals(trelloCardAfterMap.getName(), trelloCardDto.getName());
-        assertEquals(trelloCardAfterMap.getDescription(), trelloCardDto.getDescription());
-        assertEquals(trelloCardAfterMap.getPos(), trelloCardDto.getPos());
-        assertEquals(trelloCardAfterMap.getIdList(), trelloCardDto.getIdList());
+        assertEquals("card test", trelloCardAfterMap.getName());
+        assertEquals("testing...", trelloCardAfterMap.getDescription());
+        assertEquals("position in test!", trelloCardAfterMap.getPos());
+        assertEquals("267ehbdhdshwe7", trelloCardAfterMap.getIdList());
     }
 }
